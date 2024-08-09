486239833d97caff1a3e9d57df890e8577a5f44a
jilio <jilio.stories@gmail.com>
2024-08-09 15:11:02 +1000

    Single writer per sqlitefs instance

--- sqlitefs.go
+++ sqlitefs.go
@@ -3,30 +3,47 @@ package sqlitefs
 import (
 	"database/sql"
 	"errors"
-	"net/http"
+	"io/fs"
+	"sync"
 )
 
-// SQLiteFS реализует интерфейс http.FileSystem для файлов, хранящихся в SQLite.
-type SQLiteFS struct {
-	db *sql.DB
+type writeRequest struct {
+	path     string
+	data     []byte
+	index    int
+	mimeType string
+	respCh   chan error
 }
+
+type SQLiteFS struct {
+	db       *sql.DB
+	writeCh  chan writeRequest
+	writerWg sync.WaitGroup
+}
+
+var _ fs.FS = (*SQLiteFS)(nil)
 
 // NewSQLiteFS создает новый экземпляр SQLiteFS с заданной базой данных.
 // Проверяет наличие необходимых таблиц и создает их при отсутствии.
 func NewSQLiteFS(db *sql.DB) (*SQLiteFS, error) {
-	fs := &SQLiteFS{db: db}
+	fs := &SQLiteFS{
+		db:      db,
+		writeCh: make(chan writeRequest),
+	}
 
-	// Создание таблиц, если они не существуют
 	err := fs.createTablesIfNeeded()
 	if err != nil {
 		return nil, err
 	}
 
+	fs.writerWg.Add(1)
+	go fs.writerLoop()
+
 	return fs, nil
 }
 
 // Open открывает файл по указанному пути.
-func (fs *SQLiteFS) Open(name string) (http.File, error) {
+func (fs *SQLiteFS) Open(name string) (fs.File, error) {
 	// Проверка существования файла в базе данных
 	var exists bool
 	err := fs.db.QueryRow("SELECT EXISTS(SELECT 1 FROM file_metadata WHERE path = ?)", name).Scan(&exists)
@@ -62,4 +79,51 @@ func (fs *SQLiteFS) createTablesIfNeeded() error {
     `)
 
 	return err
+}
+
+func (fs *SQLiteFS) writerLoop() {
+	defer fs.writerWg.Done()
+
+	for req := range fs.writeCh {
+		var err error
+		if req.mimeType != "" {
+			err = fs.createFileRecord(req.path, req.mimeType)
+		} else {
+			err = fs.writeFragment(req.path, req.data, req.index)
+		}
+		req.respCh <- err
+	}
+}
+
+func (fs *SQLiteFS) createFileRecord(path, mimeType string) error {
+	_, err := fs.db.Exec("INSERT OR REPLACE INTO file_metadata (path, type) VALUES (?, ?)", path, mimeType)
+	return err
+}
+
+func (fs *SQLiteFS) writeFragment(path string, data []byte, index int) error {
+	tx, err := fs.db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	var fileID int64
+	err = tx.QueryRow("SELECT id FROM file_metadata WHERE path = ?", path).Scan(&fileID)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec("INSERT OR REPLACE INTO file_fragments (file_id, fragment_index, fragment) VALUES (?, ?, ?)",
+		fileID, index, data)
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func (fs *SQLiteFS) Close() error {
+	close(fs.writeCh)
+	fs.writerWg.Wait()
+	return fs.db.Close()
 }
