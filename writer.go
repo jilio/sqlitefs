45c0baebe5966f6a96b13da65233dce8ce81ba5d
jilio <jilio.stories@gmail.com>
2025-04-21 00:18:50 +1000

    feat: simpler writer and proper close

--- writer.go
+++ writer.go
@@ -1,6 +1,7 @@
 package sqlitefs
 
 import (
+	"errors"
 	"mime"
 	"path/filepath"
 )
@@ -14,8 +15,11 @@ type SQLiteWriter struct {
 	fragmentSize  int
 	fragmentIndex int
 	fileCreated   bool
+	closed        bool
 }
 
+// NewSQLiteWriter creates a new SQLiteWriter for the specified path.
+// Deprecated: Use SQLiteFS.NewWriter instead.
 func NewSQLiteWriter(fs *SQLiteFS, path string) *SQLiteWriter {
 	return &SQLiteWriter{
 		fs:           fs,
@@ -26,6 +30,10 @@ func NewSQLiteWriter(fs *SQLiteFS, path string) *SQLiteWriter {
 }
 
 func (w *SQLiteWriter) Write(p []byte) (n int, err error) {
+	if w.closed {
+		return 0, errors.New("sqlitefs: write to closed writer")
+	}
+
 	n = len(p)
 	w.buffer = append(w.buffer, p...)
 
@@ -84,11 +92,17 @@ func (w *SQLiteWriter) createFileRecord() error {
 }
 
 func (w *SQLiteWriter) Close() error {
+	if w.closed {
+		return nil
+	}
+
 	for len(w.buffer) > 0 || !w.fileCreated {
 		err := w.writeFragment()
 		if err != nil {
 			return err
 		}
 	}
+
+	w.closed = true
 	return nil
 }
